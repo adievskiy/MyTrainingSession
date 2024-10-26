c089d63db1f66e95b45b3f61f4b597000b081b1f
adievskiy <dievskiy@mail.ru>
2024-10-26 23:04:13 +0400

    Complete

--- app/src/main/java/com/example/mytrainingsession/ExerciseChoose.kt
+++ app/src/main/java/com/example/mytrainingsession/ExerciseChoose.kt
@@ -9,7 +9,6 @@ import android.view.MenuItem
 import android.view.View
 import android.widget.LinearLayout
 import android.widget.TextView
-import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
